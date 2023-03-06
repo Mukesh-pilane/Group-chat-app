301ff3e31283d727c02a5ecc57c163cc76f0e964
Mukesh-pilane <mukeshpilane@gmail.com>
2023-03-07 02:03:59 +0530

    application setup

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from app import create_app
-
+from flask import request
+from flask_socketio import SocketIO, emit
 app = create_app()
+socketio = SocketIO(app,cors_allowed_origins="*")
+@socketio.on('connect')
+def test_connect():
+    print("Connected")
+    emit("connect",{"data":f"id: {request.sid} is connected"})
+    emit("res", {"data": "dghdh"});
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+
 
 if __name__ == '__main__':
-    app.run()
-
+    socketio.run(app, debug=True,port=5001)
--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 import sys
 import os
-from flask import Flask
-
+from flask import Flask, request
 from flask_cors import CORS
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), './')))
 import resources 
 def create_app():
     app = Flask(__name__)
-    cors = CORS(app)
+    CORS(app,resources={r"/*":{"origins":"*"}})
+    
     return app

569aa7bf137f213b27ddb15d2c71cc7b5a8a2782
Mukesh-pilane <mukeshpilane@gmail.com>
2023-03-06 18:56:48 +0530

    application setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,21 @@
-
-
+import io from 'socket.io-client';
+import {useEffect} from 'react';
 function App() {
+  useEffect(()=>{
+   const socket = io("localhost:5001/", {
+        transports: ["websocket"],
+        cors: {
+          origin: "http://localhost:3000/",
+        },
+      });
+ socket.on("connect", (data) => {
+        console.log(data, "data");
+      }); 
+ socket.on("res", (data)=>{
+   console.log(data)
+ })
+  }, [])
+
   return (
     <div className="App">
     Frontend
